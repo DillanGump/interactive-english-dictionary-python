526c1b491ae4f5d56d597ab8cde07ff91b1d368c
Dillan Gump <gump.31@buckeyemail.osu.edu>
2020-04-15 08:01:40 -0400

    Implements get_close_matches to suggest a key that exists

--- dictionaryApp1.py
+++ dictionaryApp1.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 dictPath = "data/data.json"
 data = json.load(open(dictPath))
 
@@ -6,6 +7,8 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys()) [0]
     else:
         return "The word doesn't exist. Please double check it"
     
