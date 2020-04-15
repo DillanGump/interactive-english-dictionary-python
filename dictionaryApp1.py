9f583d864752f95648b0ab59d1bead5f35a02ea7
Dillan Gump <gump.31@buckeyemail.osu.edu>
2020-04-15 07:34:24 -0400

    Adds functionality to accept capitalization not in data set

--- dictionaryApp1.py
+++ dictionaryApp1.py
@@ -3,7 +3,11 @@ dictPath = "data/data.json"
 data = json.load(open(dictPath))
 
 def translate(word):
-    return data[word]
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return "The word doesn't exist. Please double check it"
     
 word = input("Enter word: ")
 
